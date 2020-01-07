9971daddd55c2e5c6893a5488e0a93c285037e74
Harphies <tafeez.tech@gmail.com>
2020-01-07 19:57:14 +0100

    Updates

--- precache-manifest.c2e12b792bc14c74e840725ebcbc5e5e.js
+++ precache-manifest.c2e12b792bc14c74e840725ebcbc5e5e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3210c37e0cef0583aa339e9d5c0b7245",
+    "revision": "eba451dc441c74de2dbe1d9f14f6f66a",
     "url": "/home/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/css/2.86aa6515.chunk.css"
   },
   {
-    "revision": "101f04d10af057eff29d",
+    "revision": "8d5574010c380c261a03",
     "url": "/home/static/css/main.6fd842a9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.3987edfa.chunk.js"
   },
   {
-    "revision": "101f04d10af057eff29d",
-    "url": "/home/static/js/main.0416c653.chunk.js"
+    "revision": "8d5574010c380c261a03",
+    "url": "/home/static/js/main.02066229.chunk.js"
   },
   {
     "revision": "7325590a58cbc13614ef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.3441d6e5ec0945719f8275e29fa90d02.js"
+  "/home/precache-manifest.c2e12b792bc14c74e840725ebcbc5e5e.js"
 );
 
 self.addEventListener('message', (event) => {
