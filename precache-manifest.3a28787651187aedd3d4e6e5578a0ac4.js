069856249b401c61711900b1213018aedd98aa47
Harphies <tafeez.tech@gmail.com>
2020-01-07 17:53:56 +0100

    Updates

---
+++ precache-manifest.3a28787651187aedd3d4e6e5578a0ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7884aab150e45ecef300b671ed118d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "51c5000c8e853deb796b",
+    "url": "/home/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "d50ccb1c2b00073c49ed",
+    "url": "/home/static/css/main.6fd842a9.chunk.css"
+  },
+  {
+    "revision": "51c5000c8e853deb796b",
+    "url": "/home/static/js/2.3987edfa.chunk.js"
+  },
+  {
+    "revision": "d50ccb1c2b00073c49ed",
+    "url": "/home/static/js/main.1e10900d.chunk.js"
+  },
+  {
+    "revision": "7325590a58cbc13614ef",
+    "url": "/home/static/js/runtime~main.9cfbdb8c.js"
+  }
+]);
